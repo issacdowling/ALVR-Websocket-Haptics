fbe84ee3a07c298409c70e83e8150e22281fda56
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2023-03-05 13:25:08 +0000

    Send vibration speed from PC

--- main.ino
+++ main.ino
@@ -4,16 +4,19 @@ int LED = 2; // Set LED Pin to 2
 void setup() {
     Serial.begin(9600); // open the serial port at 9600 bps:
     pinMode(LED, OUTPUT);
-
+    Serial.setTimeout(100000);
 }
 
 void loop() {
 
-    for(int duty_cycle = 255;  duty_cycle > 115; duty_cycle--) {
-        Serial.print(duty_cycle);
-        Serial.print("\n");
-        analogWrite(LED, duty_cycle); // Set LED speed
-        delay(200); // wait
-    }
-
+    // for(int duty_cycle = 255;  duty_cycle > 113; duty_cycle--) {
+    //     Serial.print(duty_cycle);
+    //     Serial.print("\n");
+    //     analogWrite(LED, 120); // Set LED speed
+    //     delay(200); // wait
+    // }
+    long myInt = Serial.parseInt();
+    Serial.print(myInt);
+    Serial.print("\n");
+    analogWrite(LED, myInt); // Set LED speed
 }
