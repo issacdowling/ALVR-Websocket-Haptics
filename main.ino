13af97d9f551470bb32ff72af9f8fa874643c0fc
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2023-03-10 06:55:28 +0000

    Change Pin

--- main.ino
+++ main.ino
@@ -1,4 +1,4 @@
-int LED = 2; // Set LED Pin to 2
+int MOTOR = 16; // Set MOTOR Pin to 16
 
 // Must be floats not ints, or division happens weirdly
 float min_haptic_value = 120; // Set the minimum threshold for "on" to 110 of 255
@@ -9,18 +9,18 @@ void vibe(int duration, int intensity){
   //Normalise intensity from 0-100 to range 120-255
   pwm_value = min_haptic_value+(intensity*((max_haptic_value-min_haptic_value)/100));
   //Jumpstart the motor at max intensity for 20ms to allow starting at low intensities
-  analogWrite(LED, 255);
+  analogWrite(MOTOR, 255);
   delay(20);
   //Set motor to selected intensity for as long as input
-  analogWrite(LED, pwm_value); // Set LED speed
+  analogWrite(MOTOR, pwm_value); // Set MOTOR speed
   delay(duration);
   //After duration complete, stop motor
-  analogWrite(LED, 0);
+  analogWrite(MOTOR, 0);
 }
 
 void setup() {
     Serial.begin(9600); // open the serial port at 9600 bps:
-    pinMode(LED, OUTPUT);
+    pinMode(MOTOR, OUTPUT);
     Serial.setTimeout(100000);
 }
 
