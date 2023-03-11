f273dfa17b850f39b1da351609daa8dfb5563506
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2023-03-11 09:39:15 +0000

    Add debouncing

--- main.ino
+++ main.ino
@@ -23,28 +23,97 @@ const char* PATH = "/user/hand/right";
 using namespace websockets;
 
 void vibe(int duration, int intensity){
-  //Normalise intensity from 0-100 to range 120-255
-  pwm_value = MIN_HAPTIC_VALUE+(intensity*((MAX_HAPTIC_VALUE-MIN_HAPTIC_VALUE)/100));
-  //Jumpstart the motor at max intensity for 20ms when at low intensities
-  if(intensity<20){
+
+  now_called_millis = millis();
+
+  // Debounces
+  if (last_called_millis - now_called_millis < 50) {
+    last_called_millis = now_called_millis;
+
+    Serial.println(duration);
+    Serial.println(intensity);
+
+    //Normalise intensity from 0-100 to range 120-255
+    pwm_value = MIN_HAPTIC_VALUE+(intensity*((MAX_HAPTIC_VALUE-MIN_HAPTIC_VALUE)/100));
+    //Jumpstart the motor at max intensity for 20ms when at low intensities
     analogWrite(MOTOR, 255);
     delay(20);
-    if (duration<50){
-      duration = 50;
-    }
-    if (intensity<10){
-      intensity = 10;
-    }
+    //Set motor to selected intensity for as long as input
+    analogWrite(MOTOR, pwm_value); // Set MOTOR speed
+    delay(duration);
+    //After duration complete, stop motor
+    analogWrite(MOTOR, 0);
+
   }
-  if(duration<20){
-    duration = 20;
-  }
-  
-  //Set motor to selected intensity for as long as input
-  analogWrite(MOTOR, pwm_value); // Set MOTOR speed
-  delay(duration);
-  //After duration complete, stop motor
-  analogWrite(MOTOR, 0);
+
+}
+
+void onMessageCallback(WebsocketsMessage msg) {
+  //PARSE JSON
+    
+    String message = msg.data();
+    // Deserialize the JSON document IF not empty
+    if(!msg.isEmpty()){
+      StaticJsonDocument<999> json;
+      DeserializationError error = deserializeJson(json, message);
+
+      // Test if parsing succeeds.
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+      }
+
+      //CHECK WHETHER MESSAGE IS FOR THIS DEVICE
+      //Check if a haptic message
+      String id = json["id"];
+      if (id=="Haptics") {
+        //Check that the message is for the correct hand
+        String recieved_path = json["data"]["path"];
+        if (recieved_path==PATH){
+          String length = json["data"]["duration"]["nanos"];
+          String amplitude = json["data"]["amplitude"];
+          vibe((length.toInt()/1000000), (amplitude.toInt()*100));
+        }
+
+      }
+    }
+
+
+}
+
+
+void onEventsCallback(WebsocketsEvent event, String data) {
+    if(event == WebsocketsEvent::ConnectionOpened) {
+        Serial.println("Connnection Opened");
+    } else if(event == WebsocketsEvent::ConnectionClosed) {
+        Serial.println("Connnection Closed");
+    }
+}
+
+WebsocketsClient client;
+void setup() {
+    pinMode(MOTOR, OUTPUT);
+    Serial.begin(115200);
+    // Connect to wifi
+    WiFi.begin(SSID, PASSWORD);
+
+    // Wait some time to connect to wifi
+    for(int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
+        Serial.print(".");
+        delay(1000);
+    }
+
+    // Setup Callbacks
+    client.onMessage(onMessageCallback);
+    client.onEvent(onEventsCallback);
+    
+    // Connect to server
+    client.connect(ALVR_ENDPOINT);
+}
+
+void loop() {
+    client.poll();
 }
 
 void onMessageCallback(WebsocketsMessage msg) {
